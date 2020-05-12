be9ea671b595f11ead79280940a72600415184fc
Rohan Khurana <47374538+rohankhdbzmf@users.noreply.github.com>
2020-05-12 23:46:29 +0530

    Create single_element_in_array.cpp
    You are given a sorted array consisting of only integers where every element appears exactly twice, except for one element which appears exactly once. Find this single element that appears only once.
    Note: Your solution should run in O(log n) time and O(1) space.

---
+++ single_element_in_array.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) 
+    {
+        if(nums.size()==1)
+            return nums[0];
+        int low=0;
+        int high=nums.size()-1;
+        while(low<high)
+        {
+            int mid=(low+high)/2;
+            if (mid%2 == 0) 
+            { 
+                if (nums[mid] == nums[mid+1]) 
+                    low=mid+2;
+                else
+                    high=mid; 
+            } 
+            else
+            { 
+                if (nums[mid] ==nums[mid-1]) 
+                    low=mid+1;
+                else
+                    high=mid-1;
+            } 
+        }
+        return nums[low];
+    }
+    /*
+      //complexity O(n):
+      int singleNonDuplicate(vector<int>& nums) {
+      int result = 0;
+      for(int i = 0;i < nums.size(); i++)
+        result ^= nums[i];
+      return result;
+   }*/
+};
