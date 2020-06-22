5da3159498622712eb2be8df389919aad01dd073
Rohan Khurana <47374538+rohankhdbzmf@users.noreply.github.com>
2020-06-22 16:44:19 +0530

    Create Single Number II.java
    Single Number II

---
+++ LeetCode_Monthly_Challenge/June/Java/Week_4/Single Number II.java
@@ -0,0 +1,30 @@
+/*
+PROBLEM:
+Given a non-empty array of integers, every element appears three times except for one, which appears exactly once. Find that single one.
+Note:
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+Example 1:
+Input: [2,2,3,2]
+Output: 3
+
+Example 2:
+Input: [0,1,0,1,0,1,99]
+Output: 99
+*/
+
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ones = 0, twos = 0, thrice = 0; 
+        for(int n:nums)
+        { 
+        twos = twos | (ones & n);  
+        ones = ones^n;  
+        thrice = ~(ones & twos);  
+        ones &= thrice;  
+        twos &= thrice;
+        }  
+    return ones;  
+    }
+}
