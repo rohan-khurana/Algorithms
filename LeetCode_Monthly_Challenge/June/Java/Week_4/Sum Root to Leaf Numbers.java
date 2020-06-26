86167a4c8350e657a251eac3534e7456245007b0
Rohan Khurana <47374538+rohankhdbzmf@users.noreply.github.com>
2020-06-26 17:51:18 +0530

    Create Sum Root to Leaf Numbers.java
    Sum Root to Leaf Numbers

---
+++ LeetCode_Monthly_Challenge/June/Java/Week_4/Sum Root to Leaf Numbers.java
@@ -0,0 +1,70 @@
+/*
+PROBLEM:
+Given a binary tree containing digits from 0-9 only, each root-to-leaf path could represent a number.
+An example is the root-to-leaf path 1->2->3 which represents the number 123.
+Find the total sum of all root-to-leaf numbers.
+Note: A leaf is a node with no children.
+
+Example:
+Input: [1,2,3]
+    1
+   / \
+  2   3
+Output: 25
+Explanation:
+The root-to-leaf path 1->2 represents the number 12.
+The root-to-leaf path 1->3 represents the number 13.
+Therefore, sum = 12 + 13 = 25.
+
+Example 2:
+Input: [4,9,0,5,1]
+    4
+   / \
+  9   0
+ / \
+5   1
+Output: 1026
+Explanation:
+The root-to-leaf path 4->9->5 represents the number 495.
+The root-to-leaf path 4->9->1 represents the number 491.
+The root-to-leaf path 4->0 represents the number 40.
+Therefore, sum = 495 + 491 + 40 = 1026.
+*/
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int result=0;
+    public int sumNumbers(TreeNode root) {
+        if(root==null) return 0;
+        result=0;
+        findSum(root, 0);
+        return result;
+    }
+    private void findSum(TreeNode root, int val){
+        int current=10*val + root.val;
+        if(root.left==null && root.right==null){
+            result+=current;
+            return;
+        }
+        if(root.left != null)
+            findSum(root.left, current);
+        if(root.right != null)
+            findSum(root.right, current);
+    }
+}
