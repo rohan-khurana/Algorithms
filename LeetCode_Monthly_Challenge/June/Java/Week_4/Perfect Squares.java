e619db9ba506643a918b05ac0394044e8a588226
Rohan Khurana <47374538+rohankhdbzmf@users.noreply.github.com>
2020-06-27 19:56:35 +0530

    Create Perfect Squares.java
    Perfect Squares

---
+++ LeetCode_Monthly_Challenge/June/Java/Week_4/Perfect Squares.java
@@ -0,0 +1,35 @@
+/*
+PROBLEM:
+Given a positive integer n, find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
+
+Example 1:
+Input: n = 12
+Output: 3 
+Explanation: 12 = 4 + 4 + 4.
+
+Example 2:
+Input: n = 13
+Output: 2
+Explanation: 13 = 4 + 9.
+*/
+
+
+
+
+class Solution {
+    public int numSquares(int n) {
+        if(n<=0)
+            return 0;
+        int[] dp=new int[n+1];
+        Arrays.fill(dp,0);
+        for(int i=1;i<=n;i++)
+        {
+            int minimum=i;
+            for(int j=1;j*j<=i;j++){
+                minimum=Math.min(minimum,1+dp[i-(j*j)]);
+            }
+            dp[i]=minimum;
+        }
+        return dp[n];
+    }
+}
