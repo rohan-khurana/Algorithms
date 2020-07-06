15b24bafff9240d103c16638c8596bbd381174c0
Rohan Khurana <47374538+rohankhdbzmf@users.noreply.github.com>
2020-07-06 14:51:36 +0530

    Create Plus One.cpp

--- LeetCode_Monthly_Challenge/July/C++/Week_1/Plus One.cpp
+++ LeetCode_Monthly_Challenge/July/C++/Week_1/Plus One.cpp
@@ -23,14 +23,13 @@ public:
         int carry=1;
         for(int i=digits.size()-1;i>=0;i--)
         {
-            digits[i]+=carry;
-            if(digits[i]>9){
-                digits[i]%=10;
-                carry=1;
+            if(digits[i]<9){
+                digits[i]++;
+                return digits;
             }
-            else carry=0;
+            else digits[i]=0;
         }
-        if(carry==1) digits.insert(digits.begin(),1);
+        digits.insert(digits.begin(),1);
         return digits;
     }
 };
