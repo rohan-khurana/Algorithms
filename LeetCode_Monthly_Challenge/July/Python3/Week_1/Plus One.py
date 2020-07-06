85df199e550c0cafa09f53b1e3c47f5dd78aebb5
Rohan Khurana <47374538+rohankhdbzmf@users.noreply.github.com>
2020-07-06 14:54:13 +0530

    Create Plus One.py

--- LeetCode_Monthly_Challenge/July/Python3/Week_1/Plus One.py
+++ LeetCode_Monthly_Challenge/July/Python3/Week_1/Plus One.py
@@ -19,14 +19,10 @@ Explanation: The array represents the integer 4321.
 
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        carry=1
         for i in range(len(digits)-1,-1,-1):
-            digits[i]+=carry
-            if digits[i]>9:
-                digits[i]%=10
-                carry=1
-            else:
-                carry=0
-        if carry: 
-            digits.insert(0,1)
+            if digits[i]<9:
+                digits[i]+=1
+                return digits
+            digits[i]=0
+        digits.insert(0,1)
         return digits
