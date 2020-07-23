30dd0691475f7a31893c3e89dade06960e09fc04
Rohan Khurana <47374538+rohankhdbzmf@users.noreply.github.com>
2020-07-23 20:06:29 +0530

    Create Single Number III.java
    Single Number III

---
+++ LeetCode_Monthly_Challenge/July/Java/Week_4/Single Number III.java
@@ -0,0 +1,28 @@
+/*
+PROBLEM:
+Given an array of numbers nums, in which exactly two elements appear only once and all the other elements appear exactly twice. Find the two elements that appear only once.
+
+Example:
+Input:  [1,2,1,3,2,5]
+Output: [3,5]
+
+Note:
+The order of the result is not important. So in the above example, [5, 3] is also correct.
+Your algorithm should run in linear runtime complexity. Could you implement it using only constant space complexity?
+*/
+
+
+
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int[] result={0,0};
+        int XOR = 0;
+        for(int n: nums) XOR^=n;
+        XOR &= -XOR;
+        for(int n: nums){
+            if((XOR & n)!=0) result[0]^=n;
+            else result[1]^=n;
+        }
+        return result;
+    }
+}
