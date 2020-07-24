37c8ecb5101544f3c3362ffe72e5027abdd44568
Rohan Khurana <47374538+rohankhdbzmf@users.noreply.github.com>
2020-07-24 17:32:27 +0530

    Create All Paths From Source to Target.cpp
    All Paths From Source to Target

---
+++ LeetCode_Monthly_Challenge/July/C++/Week_4/All Paths From Source to Target.cpp
@@ -0,0 +1,40 @@
+/*
+PROBLEM:
+Given a directed, acyclic graph of N nodes.  Find all possible paths from node 0 to node N-1, and return them in any order.
+The graph is given as follows:  the nodes are 0, 1, ..., graph.length - 1.  graph[i] is a list of all nodes j for which the edge (i, j) exists.
+Example:
+Input: [[1,2], [3], [3], []] 
+Output: [[0,1,3],[0,2,3]] 
+Explanation: The graph looks like this:
+0--->1
+|    |
+v    v
+2--->3
+There are two paths: 0 -> 1 -> 3 and 0 -> 2 -> 3.
+Note:
+The number of nodes in the graph will be in the range [2, 15].
+You can print different paths in any order, but you should keep the order of nodes inside one path.
+*/
+
+
+
+class Solution {
+public:
+    void dfs(vector<vector<int>>& graph, vector<int>& path, vector<vector<int>>& result) {
+        if(path.back() == graph.size() - 1) {
+            result.push_back(path);
+            return;
+        }
+        for(int next: graph[path.back()]) {
+            path.push_back(next);
+            dfs(graph, path, result);
+            path.pop_back();
+        }
+    }
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        vector<vector<int>> result;
+        vector<int> path{0};
+        dfs(graph, path, result);
+        return result;
+    }
+};
