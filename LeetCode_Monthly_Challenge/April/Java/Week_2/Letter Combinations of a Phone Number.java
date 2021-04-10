621c11bc16607784fdd7a92df17e6231f6e054dd
Rohan Khurana <47374538+rohan-khurana@users.noreply.github.com>
2021-04-10 12:17:18 +0530

    Create Letter Combinations of a Phone Number.java

---
+++ LeetCode_Monthly_Challenge/April/Java/Week_2/Letter Combinations of a Phone Number.java
@@ -0,0 +1,42 @@
+/*
+Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+Example 1:
+Input: digits = "23"
+Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+
+Example 2:
+Input: digits = ""
+Output: []
+
+Example 3:
+Input: digits = "2"
+Output: ["a","b","c"]
+
+Constraints:
+0 <= digits.length <= 4
+digits[i] is a digit in the range ['2', '9'].
+*/
+
+
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        LinkedList<String> result=new LinkedList();
+        if(digits.length()==0) return result;
+        result.add("");
+        
+        String[] keys=new String[]{"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        
+        for(int i=0;i<digits.length();i++){
+            int index=Character.getNumericValue(digits.charAt(i));
+            while(result.peek().length()==i){
+                String permutation=result.remove();
+                for(char c : keys[index].toCharArray()){
+                    result.add(permutation+c);
+                }
+            }
+        }
+        return result;
+    }
+}
